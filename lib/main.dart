2d7b341406a2f788d86d6feaf945aa2b7f4b00cb
Anish G <anish@masalabox.com>
2020-11-24 15:11:10 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import './screens/dashboard_screen.dart';
+import './screens/movie_details_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+        scaffoldBackgroundColor: Colors.white,
+      ),
+      home: DashboardScreen(),
+      routes: {
+        MovieDetailsScreen.routeName: (ctx) => MovieDetailsScreen(),
+      },
+    );
+  }
+}
